81d92621939bb7eb81c8876334276755722d6693
Alexandr <artamoshkinas@gmail.com>
2022-11-26 11:35:05 +0700

    Задание 1 выполнено!

--- Home_Work_2.py
+++ Home_Work_2.py
@@ -1,1 +1,24 @@
 
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+def sum_nambers() -> int:
+    """Сумма цифр вещественного числа"""
+
+    try:
+        number = float(input("Введите число: ").replace(',', '.'))
+        if type(number) in [int, float]:
+            a = []
+            for i in str(number):
+                a.append(i)
+            a.remove('.')
+            if str(number).endswith('.0'):
+                str(number).replace('.0', '')
+                print(f'- {str(number).replace(".0", "")} -> {sum(list(map(int, a)))}')
+            else:
+                print(f'- {number} -> {sum(list(map(int, a)))}')
+    except ValueError:
+        print('Введенное число должно быть либо целым, либо вещественным!')
+        print('Повторите ввод!')
+        return sum_nambers()
+
+
+sum_nambers()
