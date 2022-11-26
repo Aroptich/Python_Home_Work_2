67ba12908951ab597795f2f544e14ed72f9480b8
Alexandr <artamoshkinas@gmail.com>
2022-11-26 11:46:21 +0700

    Задание 3 выполнено!

--- Home_Work_2.py
+++ Home_Work_2.py
@@ -1,4 +1,3 @@
-
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 # def sum_nambers() -> int:
 #     """Сумма цифр вещественного числа"""
@@ -27,18 +26,36 @@
 
 def products_of_numbers():
     N = int(input('Введите целое число: '))
-    a =[]
+    a = []
     Factorial = 1
-    for i in range(1, N+1):
+    for i in range(1, N + 1):
         Factorial = Factorial * i
         a.append(Factorial)
     Factorial = str(1)
     b = []
     b.append(Factorial)
-    for j in range(2, N+1):
-        Factorial  += '*'+str(j)
+    for j in range(2, N + 1):
+        Factorial += '*' + str(j)
         b.append(Factorial)
     print(f'- пусть N = {N}, тогда {a} ({", ".join(b)})')
 
-products_of_numbers()
 
+# products_of_numbers()
+
+# Задайте список из n чисел последовательности (1+1/ n)^n и выведите на экран их сумму.
+
+def dict_comprehension() -> dict:
+    try:
+        n = int(input('Введите целое число >=0:'))
+        if n > 0:
+            dict = {k: round((1 + 1 / k) ** k, 2) for k in range(1, n + 1)}
+            print(f'- Для n = {n}: {dict} ')
+            return sum(dict.values())
+        return {}
+    except ValueError:
+        print('Вы ввели не целое число!')
+        print('Повторите ввод')
+        return dict_comprehension()
+
+
+print(f'Сумма {dict_comprehension()}')
